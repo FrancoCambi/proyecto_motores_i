a78f24780989e440be20d84ed89de7c56fdc4ffc
FrancoCambi <francoocambiaso@gmail.com>
2026-09-05 21:18:28 -0300

    simple player movement

---
+++ Assets/Code/Player/PlayerMovement.cs
@@ -0,0 +1,182 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [Tooltip("Move speed of the character in m/s")]
+    [SerializeField] private float moveSpeed = 2.0f;
+
+    [Tooltip("Sprint speed of the character in m/s")]
+    [SerializeField] private float sprintSpeed = 5.335f;
+
+    [Tooltip("How fast the character turns to face movement direction")]
+    [SerializeField][Range(0.0f, 0.3f)] private float rotationSmoothTime = 0.12f;
+
+    [Tooltip("Acceleration and deceleration")]
+    [SerializeField] private float speedChangeRate = 10.0f;
+
+    [Tooltip("The height the player can jump")]
+    [SerializeField][Min(0f)] private float jumpHeight = 1.2f;
+
+    [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
+    [SerializeField] private float gravity = -15.0f;
+
+    [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
+    [SerializeField] private float jumpTimeout = 0.50f;
+
+    [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
+    [SerializeField] private float fallTimeout = 0.15f;
+
+    [Header("Grounded")]
+    [Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
+    [SerializeField] private bool grounded = true;
+
+    [Tooltip("Useful for rough ground")]
+    [SerializeField] private float groundedOffset = -0.14f;
+
+    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
+    [SerializeField] private float groundedRadius = 0.28f;
+
+    [Tooltip("What layers the character uses as ground")]
+    [SerializeField] private LayerMask groundLayers;
+
+    [Header("Input References")]
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
+    [SerializeField] private InputActionReference sprintAction;
+
+    private Vector2 _moveDirection;
+
+    private bool _jump;
+    private bool _sprint;
+
+    private float _speed;
+    private float _targetRotation = 0.0f;
+    private float _rotationVelocity;
+    private float _verticalVelocity;
+    private float _terminalVelocity = 53.0f;
+
+    private float _jumpTimeoutDelta;
+    private float _fallTimeoutDelta;
+
+    private CharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+
+        _jumpTimeoutDelta = jumpTimeout;
+        _fallTimeoutDelta = fallTimeout;
+    }
+
+    private void Update()
+    {
+        ReadInput();
+        JumpAndGravity();
+        GroundedCheck();
+        Move();
+    }
+
+    private void ReadInput()
+    {
+        _moveDirection = moveAction.action.ReadValue<Vector2>();
+        _jump = jumpAction.action.WasPressedThisFrame();
+        _sprint = sprintAction.action.IsPressed();
+    }
+
+    private void GroundedCheck()
+    {
+        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset, transform.position.z);
+        grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers, QueryTriggerInteraction.Ignore);
+    }
+
+    private void Move()
+    {
+        float targetSpeed = _sprint ? sprintSpeed : moveSpeed;
+
+        if (_moveDirection == Vector2.zero) targetSpeed = 0.0f;
+
+        float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+        float speedOffset = 0.1f;
+
+        if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+        {
+            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed, Time.deltaTime * speedChangeRate);
+
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
+        }
+        else
+        {
+            _speed = targetSpeed;
+        }
+
+        Vector3 inputDirection = new Vector3(_moveDirection.x, 0.0f, _moveDirection.y).normalized;
+
+        if (_moveDirection != Vector2.zero)
+        {
+            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, rotationSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+    }
+
+    private void JumpAndGravity()
+    {
+        if (grounded)
+        {
+            _fallTimeoutDelta = fallTimeout;
+
+            // stop our velocity dropping infinitely when grounded
+            if (_verticalVelocity < 0.0f)
+            {
+                _verticalVelocity = -2f;
+            }
+
+            if (_jump && _jumpTimeoutDelta <= 0.0f)
+            {
+                _verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            // jump timeout
+            if (_jumpTimeoutDelta >= 0.0f)
+            {
+                _jumpTimeoutDelta -= Time.deltaTime;
+            }
+        }
+        else
+        {
+            _jumpTimeoutDelta = jumpTimeout;
+
+            // fall timeout
+            if (_fallTimeoutDelta >= 0.0f)
+            {
+                _fallTimeoutDelta -= Time.deltaTime;
+            }
+
+            // if we are not grounded, do not jump
+            _jump = false;
+        }
+
+        // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
+        if (_verticalVelocity < _terminalVelocity)
+        {
+            _verticalVelocity += gravity * Time.deltaTime;
+        }
+    }
+
+    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
+    }
+}
