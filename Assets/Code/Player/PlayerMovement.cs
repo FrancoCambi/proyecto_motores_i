42028598b4cfd0b1fa16d4d0265b91589656a38b
FrancoCambi <francoocambiaso@gmail.com>
2026-09-07 16:45:24 -0300

    removed jump and script cleanup

--- Assets/Code/Player/PlayerMovement.cs
+++ Assets/Code/Player/PlayerMovement.cs
@@ -1,19 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterController), typeof(PlayerAim))]
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [Tooltip("Move speed of the character in m/s")]
-    [SerializeField] private float moveSpeed = 2.0f;
-
-    [Tooltip("Sprint speed of the character in m/s")]
-
-    [SerializeField] private float aimingSpeed = 1.0f;
+    [SerializeField] private float moveSpeed = 5f;
 
     [Tooltip("Move speed while aiming")]
-    [SerializeField] private float sprintSpeed = 5.335f;
+    [SerializeField] private float aimingSpeed = 2.5f;
+
+    [Tooltip("Sprint speed of the character in m/s")]
+    [SerializeField] private float sprintSpeed = 10f;
 
     [Tooltip("How fast the character turns to face movement direction")]
     [SerializeField][Range(0.0f, 0.3f)] private float rotationSmoothTime = 0.12f;
@@ -21,86 +20,44 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("Acceleration and deceleration")]
     [SerializeField] private float speedChangeRate = 10.0f;
 
-    [Tooltip("The height the player can jump")]
-    [SerializeField][Min(0f)] private float jumpHeight = 1.2f;
-
-    [Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
-    [SerializeField] private float gravity = -15.0f;
-
-    [Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
-    [SerializeField] private float jumpTimeout = 0.50f;
-
-    [Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
-    [SerializeField] private float fallTimeout = 0.15f;
-
-    [Header("Grounded")]
-    [Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
-    [SerializeField] private bool grounded = true;
-
-    [Tooltip("Useful for rough ground")]
-    [SerializeField] private float groundedOffset = -0.14f;
-
-    [Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-    [SerializeField] private float groundedRadius = 0.28f;
-
-    [Tooltip("What layers the character uses as ground")]
-    [SerializeField] private LayerMask groundLayers;
-
     [Header("Input References")]
     [SerializeField] private InputActionReference moveAction;
-    [SerializeField] private InputActionReference jumpAction;
     [SerializeField] private InputActionReference sprintAction;
+
+    private CharacterController _controller;
+    private PlayerAim _playerAim;
 
     private Vector2 _moveDirection;
 
-    private bool _jump;
     private bool _sprint;
 
     private float _speed;
     private float _targetRotation = 0.0f;
     private float _rotationVelocity;
-    private float _verticalVelocity;
-    private float _terminalVelocity = 53.0f;
 
-    private float _jumpTimeoutDelta;
-    private float _fallTimeoutDelta;
-
-    private CharacterController _controller;
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
-
-        _jumpTimeoutDelta = jumpTimeout;
-        _fallTimeoutDelta = fallTimeout;
+        _playerAim = GetComponent<PlayerAim>();
     }
 
     private void Update()
     {
         ReadInput();
-        JumpAndGravity();
-        GroundedCheck();
         Move();
     }
 
     private void ReadInput()
     {
         _moveDirection = moveAction.action.ReadValue<Vector2>();
-        _jump = jumpAction.action.WasPressedThisFrame();
         _sprint = sprintAction.action.IsPressed();
     }
 
-    private void GroundedCheck()
-    {
-        Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - groundedOffset, transform.position.z);
-        grounded = Physics.CheckSphere(spherePosition, groundedRadius, groundLayers, QueryTriggerInteraction.Ignore);
-    }
 
     private void Move()
     {
-        PlayerAim aim = GetComponent<PlayerAim>();
-
-        bool isAiming = aim != null && aim.isAiming;
+        bool isAiming = _playerAim.isAiming;
 
         float targetSpeed = isAiming ? aimingSpeed : (_sprint ? sprintSpeed : moveSpeed);
 
@@ -134,57 +91,6 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
-        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
-    }
-
-    private void JumpAndGravity()
-    {
-        if (grounded)
-        {
-            _fallTimeoutDelta = fallTimeout;
-
-            // stop our velocity dropping infinitely when grounded
-            if (_verticalVelocity < 0.0f)
-            {
-                _verticalVelocity = -2f;
-            }
-
-            if (_jump && _jumpTimeoutDelta <= 0.0f)
-            {
-                _verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
-
-            // jump timeout
-            if (_jumpTimeoutDelta >= 0.0f)
-            {
-                _jumpTimeoutDelta -= Time.deltaTime;
-            }
-        }
-        else
-        {
-            _jumpTimeoutDelta = jumpTimeout;
-
-            // fall timeout
-            if (_fallTimeoutDelta >= 0.0f)
-            {
-                _fallTimeoutDelta -= Time.deltaTime;
-            }
-
-            // if we are not grounded, do not jump
-            _jump = false;
-        }
-
-        // apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-        if (_verticalVelocity < _terminalVelocity)
-        {
-            _verticalVelocity += gravity * Time.deltaTime;
-        }
-    }
-
-    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
-    {
-        if (lfAngle < -360f) lfAngle += 360f;
-        if (lfAngle > 360f) lfAngle -= 360f;
-        return Mathf.Clamp(lfAngle, lfMin, lfMax);
+        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime));
     }
 }
