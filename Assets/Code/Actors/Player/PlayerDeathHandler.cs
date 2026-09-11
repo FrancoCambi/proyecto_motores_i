cce3fa773ae7b55188efb81856c47cf85346388c
FrancoCambi <francoocambiaso@gmail.com>
2026-09-11 02:45:21 -0300

    minor code fix

--- Assets/Code/Actors/Player/PlayerDeathHandler.cs
+++ Assets/Code/Actors/Player/PlayerDeathHandler.cs
@@ -5,29 +5,32 @@ using UnityEngine;
 public class PlayerDeathHandler : MonoBehaviour, IDeathHandler
 {
     [Header("Possible Spawn Points")]
-    [SerializeField] private List<GameObject> spawnPoints;
+    [SerializeField] private Transform[] spawnPoints;
 
     private CharacterController _controller;
 
     private IResettable[] _resettables;
 
+    private List<Transform> _availableSpawnPoints;
+
     private void Awake()
     {
         _resettables = GetComponents<IResettable>();
         _controller = GetComponent<CharacterController>();
+        _availableSpawnPoints = new List<Transform>(spawnPoints);
     }
 
     public void Die()
     {
-        if (spawnPoints.Count == 0)
+        if (_availableSpawnPoints.Count == 0)
         {
             Destroy(gameObject);
             return;
         }
 
-        int randomIndex = Random.Range(0, spawnPoints.Count);
-        Vector3 nextSpawnPoint = spawnPoints[randomIndex].transform.position;
-        spawnPoints.RemoveAt(randomIndex);
+        int randomIndex = Random.Range(0, _availableSpawnPoints.Count);
+        Vector3 nextSpawnPoint = _availableSpawnPoints[randomIndex].transform.position;
+        _availableSpawnPoints.RemoveAt(randomIndex);
 
         foreach (IResettable resettable in _resettables)
         {
